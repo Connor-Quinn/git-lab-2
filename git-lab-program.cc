3faf469adcf73e817016c4eeceade7fd836f3043
connor-quinn <cq727118@ohio.edu>
2019-01-22 11:18:15 -0500

    git-lab-program.cc

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+ *    File: git-lab-program.cc
+ *    Author: Connor Quinn
+ *    Date: 1/22/19
+ *    Description: "Hello world" program in git
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+	cout << "hello Git!" << endl;
+	return 0;
+}
+
